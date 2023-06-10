bbf48636c53be516602d3466e12addbc4a0b0dc0
Eryk Możdżeń <ermozdzen@gmail.com>
2023-06-10 09:47:32 +0200

    node detecting hands

--- point_cloud_processing/include/pcl_converter.h
+++ point_cloud_processing/include/pcl_converter.h
@@ -6,11 +6,46 @@
 #include "pcl/point_types.h"
 #include "pcl_conversions/pcl_conversions.h"
 
-class PCLConverter : public rclcpp::Node {
-    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr point_cloud2_sub_;
-    bool ready;
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/correspondence.h>
+#include <pcl/features/normal_3d_omp.h>
+#include <pcl/features/shot_omp.h>
+#include <pcl/features/board.h>
+#include <pcl/filters/uniform_sampling.h>
+#include <pcl/recognition/cg/hough_3d.h>
+#include <pcl/recognition/cg/geometric_consistency.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/kdtree/impl/kdtree_flann.hpp>
+#include <pcl/common/transforms.h>
+#include <pcl/console/parse.h>
 
+class PCLConverter : public rclcpp::Node {
+    using PointType = pcl::PointXYZ;
+    using NormalType = pcl::Normal;
+    using RFType = pcl::ReferenceFrame;
+    using DescriptorType = pcl::SHOT352;
+
+    static constexpr float model_ss_ = 0.005f;
+    static constexpr float scene_ss_ = 0.01f;
+    static constexpr float rf_rad_ = 0.015f;
+    static constexpr float descr_rad_ = 0.02f;
+    static constexpr float cg_size_ = 0.01f;
+    static constexpr float cg_thresh_ = 5.0f;
+
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr point_cloud2_sub_;
+    bool busy;
+    pcl::PointCloud<PointType>::Ptr model;
+
+    struct Transform {
+        Eigen::Matrix3f rotation;
+        Eigen::Vector3f translation;
+    };
+
+    void save_point_cloud(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, std::string name);
     void point_cloud2_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
+    static std::vector<Transform> recognize(pcl::PointCloud<PointType>::Ptr model, pcl::PointCloud<PointType>::Ptr scene);
 
 public:
     PCLConverter();
--- point_cloud_processing/src/pcl_converter.cpp
+++ point_cloud_processing/src/pcl_converter.cpp
@@ -1,6 +1,8 @@
 #include "pcl_converter.h"
 
-PCLConverter::PCLConverter() : Node("pcl_converter"), ready{false} {
+PCLConverter::PCLConverter() : Node("pcl_converter"), busy{false}, model(new pcl::PointCloud<PointType>()) {
+
+    pcl::io::loadPCDFile("../pcl/hand_bin.pcd", *model);
 
     point_cloud2_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
         "/depth_camera/points",
@@ -9,18 +11,15 @@ PCLConverter::PCLConverter() : Node("pcl_converter"), ready{false} {
     );
 }
 
-void PCLConverter::point_cloud2_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg) {
-    pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+void PCLConverter::save_point_cloud(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, std::string name) {
+    if(!busy) {
+        busy = true;
 
-    fromROSMsg(*msg, *pcl_cloud);
+        //RCLCPP_INFO(this->get_logger(), "wchodze");
 
-    if(!ready) {
+        /*pcl::PointCloud<pcl::PointXYZ>::Ptr good_cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
-        pcl::PointCloud<pcl::PointXYZ>::Ptr good_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-
-        RCLCPP_INFO(this->get_logger(), "wchodze");
-
-        /*for(size_t i=0; i<pcl_cloud->points.size(); ++i) {
+        for(size_t i=0; i<pcl_cloud->points.size(); ++i) {
 
             if(std::isfinite(pcl_cloud->points[i].x) && std::isfinite(pcl_cloud->points[i].y) && std::isfinite(pcl_cloud->points[i].z)) {
                 if(std::abs(pcl_cloud->points[i].y)>0.0125 && pcl_cloud->points[i].z<2.9) {
@@ -52,10 +51,172 @@ void PCLConverter::point_cloud2_callback(const sensor_msgs::msg::PointCloud2::Sh
         good_cloud->width = good_cloud->points.size();*/
 
         RCLCPP_INFO(this->get_logger(), "saving PCD file...");
-        pcl::io::savePCDFileBinary("scene_3_hands_bin.pcd", *pcl_cloud);
-        RCLCPP_INFO(this->get_logger(), "PCD file saved");
+        pcl::io::savePCDFileBinary(name, *cloud);
+        RCLCPP_INFO(this->get_logger(), "PCD saved");
 
-        ready = true;
+        busy = false;
+    }
+}
+
+void PCLConverter::point_cloud2_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg) {
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+    fromROSMsg(*msg, *pcl_cloud);
+
+    if(busy) {
+        return;
     }
 
+    busy = true;
+
+    // coś długiego
+    std::vector<Transform> transforms = recognize(model, pcl_cloud);
+
+    std::cout << "Model instances found: " << transforms.size () << std::endl;
+    for (std::size_t i = 0; i < transforms.size (); ++i) {
+        std::cout << "\n    Instance " << i + 1 << ":" << std::endl;
+
+        Eigen::Matrix3f rotation = transforms[i].rotation;
+        Eigen::Vector3f translation = transforms[i].translation;
+
+        RCLCPP_INFO(this->get_logger(), "\n");
+        RCLCPP_INFO(this->get_logger(), "            | %6.3f %6.3f %6.3f |", rotation (0,0), rotation (0,1), rotation (0,2));
+        RCLCPP_INFO(this->get_logger(), "        R = | %6.3f %6.3f %6.3f |", rotation (1,0), rotation (1,1), rotation (1,2));
+        RCLCPP_INFO(this->get_logger(), "            | %6.3f %6.3f %6.3f |", rotation (2,0), rotation (2,1), rotation (2,2));
+        RCLCPP_INFO(this->get_logger(), "\n");
+        RCLCPP_INFO(this->get_logger(), "        t = < %0.3f, %0.3f, %0.3f >", translation (0), translation (1), translation (2));
+    }
+
+    busy = false;
+}
+
+std::vector<PCLConverter::Transform> PCLConverter::recognize(pcl::PointCloud<PointType>::Ptr model, pcl::PointCloud<PointType>::Ptr scene) {
+
+  pcl::PointCloud<PointType>::Ptr model_keypoints (new pcl::PointCloud<PointType> ());
+  pcl::PointCloud<PointType>::Ptr scene_keypoints (new pcl::PointCloud<PointType> ());
+  pcl::PointCloud<NormalType>::Ptr model_normals (new pcl::PointCloud<NormalType> ());
+  pcl::PointCloud<NormalType>::Ptr scene_normals (new pcl::PointCloud<NormalType> ());
+  pcl::PointCloud<DescriptorType>::Ptr model_descriptors (new pcl::PointCloud<DescriptorType> ());
+  pcl::PointCloud<DescriptorType>::Ptr scene_descriptors (new pcl::PointCloud<DescriptorType> ());
+
+  //
+  //  Compute Normals
+  //
+  pcl::NormalEstimationOMP<PointType, NormalType> norm_est;
+  norm_est.setKSearch (10);
+  norm_est.setInputCloud (model);
+  norm_est.compute (*model_normals);
+
+  norm_est.setInputCloud (scene);
+  norm_est.compute (*scene_normals);
+
+  //
+  //  Downsample Clouds to Extract keypoints
+  //
+
+  pcl::UniformSampling<PointType> uniform_sampling;
+  uniform_sampling.setInputCloud (model);
+  uniform_sampling.setRadiusSearch (model_ss_);
+  uniform_sampling.filter (*model_keypoints);
+  //std::cout << "Model total points: " << model->size () << "; Selected Keypoints: " << model_keypoints->size () << std::endl;
+
+  uniform_sampling.setInputCloud (scene);
+  uniform_sampling.setRadiusSearch (scene_ss_);
+  uniform_sampling.filter (*scene_keypoints);
+  //std::cout << "Scene total points: " << scene->size () << "; Selected Keypoints: " << scene_keypoints->size () << std::endl;
+
+  //
+  //  Compute Descriptor for keypoints
+  //
+  pcl::SHOTEstimationOMP<PointType, NormalType, DescriptorType> descr_est;
+  descr_est.setRadiusSearch (descr_rad_);
+
+  descr_est.setInputCloud (model_keypoints);
+  descr_est.setInputNormals (model_normals);
+  descr_est.setSearchSurface (model);
+  descr_est.compute (*model_descriptors);
+
+  descr_est.setInputCloud (scene_keypoints);
+  descr_est.setInputNormals (scene_normals);
+  descr_est.setSearchSurface (scene);
+  descr_est.compute (*scene_descriptors);
+
+  //
+  //  Find Model-Scene Correspondences with KdTree
+  //
+  pcl::CorrespondencesPtr model_scene_corrs (new pcl::Correspondences ());
+
+  pcl::KdTreeFLANN<DescriptorType> match_search;
+  match_search.setInputCloud (model_descriptors);
+
+  //  For each scene keypoint descriptor, find nearest neighbor into the model keypoints descriptor cloud and add it to the correspondences vector.
+  for (std::size_t i = 0; i < scene_descriptors->size (); ++i)
+  {
+    std::vector<int> neigh_indices (1);
+    std::vector<float> neigh_sqr_dists (1);
+    if (!std::isfinite (scene_descriptors->at (i).descriptor[0])) //skipping NaNs
+    {
+      continue;
+    }
+    int found_neighs = match_search.nearestKSearch (scene_descriptors->at (i), 1, neigh_indices, neigh_sqr_dists);
+    if(found_neighs == 1 && neigh_sqr_dists[0] < 0.25f) //  add match only if the squared descriptor distance is less than 0.25 (SHOT descriptor distances are between 0 and 1 by design)
+    {
+      pcl::Correspondence corr (neigh_indices[0], static_cast<int> (i), neigh_sqr_dists[0]);
+      model_scene_corrs->push_back (corr);
+    }
+  }
+  //std::cout << "Correspondences found: " << model_scene_corrs->size () << std::endl;
+
+  //
+  //  Actual Clustering
+  //
+  std::vector<Eigen::Matrix4f, Eigen::aligned_allocator<Eigen::Matrix4f> > rototranslations;
+  std::vector<pcl::Correspondences> clustered_corrs;
+
+  //
+  //  Compute (Keypoints) Reference Frames only for Hough
+  //
+  pcl::PointCloud<RFType>::Ptr model_rf (new pcl::PointCloud<RFType> ());
+  pcl::PointCloud<RFType>::Ptr scene_rf (new pcl::PointCloud<RFType> ());
+
+  pcl::BOARDLocalReferenceFrameEstimation<PointType, NormalType, RFType> rf_est;
+  rf_est.setFindHoles (true);
+  rf_est.setRadiusSearch (rf_rad_);
+
+  rf_est.setInputCloud (model_keypoints);
+  rf_est.setInputNormals (model_normals);
+  rf_est.setSearchSurface (model);
+  rf_est.compute (*model_rf);
+
+  rf_est.setInputCloud (scene_keypoints);
+  rf_est.setInputNormals (scene_normals);
+  rf_est.setSearchSurface (scene);
+  rf_est.compute (*scene_rf);
+
+  //  Clustering
+  pcl::Hough3DGrouping<PointType, PointType, RFType, RFType> clusterer;
+  clusterer.setHoughBinSize (cg_size_);
+  clusterer.setHoughThreshold (cg_thresh_);
+  clusterer.setUseInterpolation (true);
+  clusterer.setUseDistanceWeight (false);
+
+  clusterer.setInputCloud (model_keypoints);
+  clusterer.setInputRf (model_rf);
+  clusterer.setSceneCloud (scene_keypoints);
+  clusterer.setSceneRf (scene_rf);
+  clusterer.setModelSceneCorrespondences (model_scene_corrs);
+
+  //clusterer.cluster (clustered_corrs);
+  clusterer.recognize (rototranslations, clustered_corrs);
+
+  std::vector<Transform> transformations;
+
+  for(std::size_t i = 0; i < rototranslations.size (); ++i) {
+    Transform transformation;
+    transformation.rotation = rototranslations[i].block<3,3>(0, 0);
+    transformation.translation = rototranslations[i].block<3,1>(0, 3);
+    transformations.push_back(transformation);
+  }
+
+  return transformations;
 }
